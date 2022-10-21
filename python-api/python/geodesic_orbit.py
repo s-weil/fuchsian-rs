4e785ea0d9094e5ea46b253cedb163af7c31a677
snusnusnu <steffen.weil.ch@gmail.com>
2022-10-21 17:42:19 +0200

    further plot

--- python-api/python/geodesic_orbit.py
+++ python-api/python/geodesic_orbit.py
@@ -10,14 +10,13 @@ print(elliptic)
 modular_group = [horocyclic, elliptic]
 
 geodesic_end_points = (-1.0, 1.0) # two fixed points
-geodesic_orbit = python_api.geodesic_orbit(modular_group, geodesic_end_points, 1_000, 20, "random")
+geodesic_orbit = python_api.geodesic_orbit(modular_group, geodesic_end_points, 10_000, 20, "random")
 print("generated geodesic orbit")
 
 
 ###### PLOTTING ######
 
 import plotly.graph_objects as go
-import plotly.express as px 
 
 fig = go.Figure()
 
